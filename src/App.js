feed150bdc849223a28ff92361c625ec5b2cb56f
Nagastuma <40513995+NagatsumaHiroki@users.noreply.github.com>
2021-08-04 02:13:28 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import "./styles.css";
+import { Home } from "./Home";
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+import { Page1 } from "./Page1";
+import { Page1DetailA } from "./Page1DetailA";
+import { Page1DetailB } from "./Page1DetailB";
+import { Page2 } from "./Page2";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/page1">Page1</Link>
+        <br />
+        <Link to="/page2">Page2</Link>
+        <br />
+      </div>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route
+          path="/page1"
+          render={() => (
+            <Switch>
+              <Route exact path="/page1">
+                <Page1 />
+              </Route>
+              <Route exact path="/page1/detailA">
+                <Page1DetailA />
+              </Route>
+              <Route exact path="/page1/detailB">
+                <Page1DetailB />
+              </Route>
+            </Switch>
+          )}
+        />
+        <Route path="/page2">
+          <Page2 />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
