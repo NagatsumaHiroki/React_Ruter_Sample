caa5bb21e8b24a9684d6e8db540152704d56ea9b
Nagastuma <40513995+NagatsumaHiroki@users.noreply.github.com>
2021-08-08 09:15:51 +0900

    2021_08_08 404ページ作成

--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,7 +1,14 @@
+import { useLocation, useHistory } from "react-router-dom";
+
 export const Page1DetailA = () => {
+  const { state } = useLocation();
+
+  const history = useHistory();
+
   return (
     <div>
       <h1>Page1DatailAです</h1>
+      <button onClick={() => history.goBack()}>Go Back</button>
     </div>
   );
 };
